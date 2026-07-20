40b7c0c2b9e190d2c53b7d1e4fa182316ce4760a
TheGilly <CJordan30@belvederecollege.ie>
2026-07-20 13:55:05 -0500

    Add files via upload

--- Word Jumble Game.py
+++ Word Jumble Game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+from tkinter import messagebox
 
 
 Riddles = {"What comes down but never goes up?":"Rain",  "What word contains 26 letters but only has three syllables?":"Alphabet", "What has a head but no brain?":"Lettuce", "What is more useful when it is broken?":"Egg", "The more you take, the more you leave behind. What am I?":"Footsteps"} 
@@ -8,11 +9,43 @@ questions = []
 for i in Riddles.keys():
     questions.append(i)
 currentquestion = questions[0]
+currentanswer = ""
+count = 0
+score = 0
+
 def start_game():
-    global currentquestion, questions, Riddles
+    global currentquestion, questions, Riddles, currentanswer, count
+    count = count + 1
     QuestionLabel.config(text = currentquestion)
     currentanswer = Riddles[currentquestion]
-    AnswerLabel.config(text = currentanswer)
+    AnswerList = list(currentanswer)
+    random.shuffle(AnswerList)
+    shuffledanswer = "".join(AnswerList)
+    AnswerLabel.config(text = shuffledanswer)
+    if count == 5:
+        NextButton.config(state = "disabled")
+        
+
+
+def next_question():
+    global currentquestion, questions, Riddles, currentanswer, count, score
+    
+    
+    youranswer = entrybox.get()
+    if youranswer.lower() == currentanswer.lower():
+        messagebox.showinfo("Correct", "You entered the right answer")
+        score = score + 1
+    else:
+        messagebox.showinfo("Incorrect","You entered the wrong answer")
+    entrybox.delete(0,END)
+    nextindex = questions.index(currentquestion) + 1
+    currentquestion = questions[nextindex]
+    currentanswer = Riddles[currentquestion]
+    start_game()
+
+def submit_game():
+    global score
+    messagebox.showinfo("Result","Your score out of 5 is "+str(score)) 
     
     
     
@@ -34,10 +67,10 @@ AnswerLabel.place(x = 100, y = 200)
 StartButton = Button(text = "Start", width = 15, height = 2, bg = "green", fg = "red", font = (16), command = start_game)
 StartButton.place(x = 20, y = 400)
 
-NextButton = Button(text = "Next", width = 15, height = 2, bg = "yellow", fg = "red", font = (16))
+NextButton = Button(text = "Next", width = 15, height = 2, bg = "yellow", fg = "red", font = (16), command = next_question)
 NextButton.place(x = 220, y = 400)
 
-SubmitButton = Button(text = "Submit", width = 15, height = 2, bg = "blue", fg = "red", font = (16))
+SubmitButton = Button(text = "Submit", width = 15, height = 2, bg = "blue", fg = "red", font = (16), command = submit_game)
 SubmitButton.place(x = 420, y = 400)
 
 entrybox = Entry(width = 30, bg = "white", fg = "black")
